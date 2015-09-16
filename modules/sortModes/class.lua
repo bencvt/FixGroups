86b5788cf0b9814b4e3628d336da7f9e112ff80f
bencvt <bencvt+github@gmail.com>
2015-09-16 12:32:25 -0400

    Rework class sort mode to make groups even more homogeneous

--- modules/sortModes/class.lua
+++ modules/sortModes/class.lua
@@ -38,41 +38,45 @@ local function assignGroups(keys, players)
   -- Assign groups, class-by-class.
   wipe(R.assignedGroup)
   local curGroup, curGroupSize = 1, 0
+  -- First pass: classes with exactly 5/10/15/etc. players.
+  for _, class in ipairs(CLASS_SORT_ORDER) do
+    if counts[class] and counts[class] > 0 and counts[class] % 5 == 0 then
+      counts[class] = 0
+      curGroup, curGroupSize = assignClassToGroups(curGroup, curGroupSize, keys, players, class)
+    end
+  end
+  -- Second pass: classes with some other number of players.
   for _, class in ipairs(CLASS_SORT_ORDER) do
     if counts[class] and counts[class] > 0 then
-      -- Assign all players of this class to groups.
       counts[class] = 0
       curGroup, curGroupSize = assignClassToGroups(curGroup, curGroupSize, keys, players, class)
 
-      -- If there weren't exactly 5/10/15/etc. players, attempt to find another
-      -- class to share the last group with.
-      if curGroupSize > 0 then
-        local found
-        -- First pass: find a perfect match.
+      -- Attempt to find another class to share the last group with.
+      local found
+      -- First pass: find a perfect match.
+      for _, otherClass in ipairs(CLASS_SORT_ORDER) do
+        if not found and counts[otherClass] and curGroupSize + (counts[otherClass] % 5) == 5 then
+          found = true
+          counts[otherClass] = 0
+          curGroup, curGroupSize = assignClassToGroups(curGroup, curGroupSize, keys, players, otherClass)
+        end
+      end
+      if not found then
+        -- Second and third passes: add in an under-represented class, then try to find a perfect match again.
         for _, otherClass in ipairs(CLASS_SORT_ORDER) do
-          if not found and counts[otherClass] and curGroupSize + (counts[otherClass] % 5) == 5 then
+          if not found and counts[otherClass] and counts[otherClass] > 0 and counts[otherClass] + curGroupSize < 5 then
             found = true
             counts[otherClass] = 0
             curGroup, curGroupSize = assignClassToGroups(curGroup, curGroupSize, keys, players, otherClass)
           end
         end
-        if not found then
-          -- Second and third passes: add in an under-represented class, then try to find a perfect match again.
+        if found then
+          found = false
           for _, otherClass in ipairs(CLASS_SORT_ORDER) do
-            if not found and counts[otherClass] and counts[otherClass] > 0 and counts[otherClass] + curGroupSize < 5 then
+            if not found and counts[otherClass] and curGroupSize + (counts[otherClass] % 5) == 5 then
               found = true
               counts[otherClass] = 0
               curGroup, curGroupSize = assignClassToGroups(curGroup, curGroupSize, keys, players, otherClass)
-            end
-          end
-          if found then
-            found = false
-            for _, otherClass in ipairs(CLASS_SORT_ORDER) do
-              if not found and counts[otherClass] and curGroupSize + (counts[otherClass] % 5) == 5 then
-                found = true
-                counts[otherClass] = 0
-                curGroup, curGroupSize = assignClassToGroups(curGroup, curGroupSize, keys, players, otherClass)
-              end
             end
           end
         end
